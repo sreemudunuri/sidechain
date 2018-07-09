761db4575aeaf2b7af27100177b010a87d02accc
Sandy Edwards <sedwardscode@gmail.com>
2018-07-09 11:45:51 -0400

    create index and show page for song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,20 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :create, :edit,:update, :destroy]
+  before_action :all_songs, only: [:index]
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def all_songs
+    @songs = Song.all
+  end
 end
