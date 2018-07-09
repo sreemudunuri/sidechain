6135549ec6bb1d3242137cbb8ca50685dc3b982f
Sree <harsha6raju@yahoo.com>
2018-07-09 11:17:12 -0400

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+User.create(name: 'Drake')
+User.create(name: 'Kanye')
+User.create(name: 'Slowbeam')
+
+Song.delete_all
+
+Song.create(name: "Hot Line Bling", user_id: 1)
+Song.create(name: "Gold Digger", user_id: 2)
+Song.create(name: "Bike Ride", user_id: 3)
+Song.create(name: "Gods Plan", user_id: 1)
+Song.create(name: "Famous", user_id: 2)
+
+Comment.delete_all
+
+Comment.create(content: "Improve your vocals", user_id: 3, song_id: 2)
+Comment.create(content: "cool", user_id: 3, song_id: 1)
+Comment.create(content: "Needs more bass", user_id: 3, song_id: 4)
+Comment.create(content: "I sing better than you", user_id: 1, song_id: 2)
+Comment.create(content: "I am the next president", user_id: 2, song_id: 5)
