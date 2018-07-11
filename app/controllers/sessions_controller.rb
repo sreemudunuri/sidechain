29b586b714337bf782c97a477c5b1d9b13ec84b5
Sree <harsha6raju@yahoo.com>
2018-07-11 14:21:38 -0400

    minor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def new
 
   end
-
+ 
   def create
     @user = User.find_by(name: user_params[:name])
     if @user && @user.authenticate(user_params[:password])
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   resources :follows
   resources :song_tags
   resources :tags
-  resources :comments
+
   resources :songs
   resources :users
   resources :sessions, only: [:new, :create]
 
-  delete '/logout', to: 'sessions#destroy', as: "logout" 
+  delete '/logout', to: 'sessions#destroy', as: "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post "/user/:id/follow", to: 'users#follow'
-  post "/user/:id/unfollow", to: 'users#unfollow'
+  post "/users/:id/follow", to: 'users#follow'
+  post "/users/:id/unfollow", to: 'users#unfollow'
 
 end
